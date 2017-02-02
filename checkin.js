56b06c96c825e12e3d858a61ffa685b334c7d1cf
Ozzie <ozziesmith92@gmail.com>
2017-02-01 20:03:36 -0500

    added files

---
+++ checkin.js
@@ -0,0 +1,43 @@
+let lat;
+let lng;
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    lat = latitude;
+    lng = longitude;
+    initMap();
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+}
+
+function initMap() {
+  var uluru = {lat: lat, lng: lng};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
